6c1ce837539c55414458498a81d1b3cf88e4e7d9
SanthiyaManickam1 <santhiyamanickam2001@gmail.com>
2022-10-04 08:20:12 -0700

    commit message

---
+++ file.js
@@ -0,0 +1,56 @@
+//Arithmetic Operators
+let a = 10;
+let b = 20;
+let c = a+b;
+let c1 = a-b;
+let c2 = a*b;
+let c3 = a/b;
+let c4 = a%b;
+console.log(c,c1,c2,c3,c4) ;
+
+//Assignment Operator
+let num1 = 40;
+let num2 = 60;
+let relation = num1>=num2;
+let data = num1!=num2;
+let  plus = num1+=num2;
+let minus = num1-=num2;
+console.log(relation,data);
+
+//Logical AND Logical OR Operators
+var n1 = 100;
+var n2 = 50;
+var value = n1&&n2;
+var value1 = n1||n2;
+console.log(value,value1);
+
+//Increment  ++ and Decrement -- operator
+var a1 = 50;
+var a2 = 80;
+var inc= a1++ + ++a2 - ++a1 + a2++;
+var dec = a1-- + --a2 - --a1;
+console.log(inc,dec);
+
+//Bitwise AND Bitwise OR
+let b1 = 60;
+let b2 = 80;
+let bit = b1&b2;
+let wise = b1&b2;
+console.log(bit,wise);
+
+//Conditional Operator
+let s1 = 79;
+let s2 = 65;
+let s3 = (s1>s2) ? s1 :s2;
+console.log(s3);
+
+//Relational Operator
+var m1 = 67;
+var m2 = 58;
+var m3 = m1>>m2;
+var m4 = m1<<m2;
+console.log(m3,m4);
+
+
+
+
